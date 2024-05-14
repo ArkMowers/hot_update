143475691ef6ed70b839cc77bd9286d14de84fdc
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2024-05-15 04:42:17 +0800

    删除过期的任务

--- sign_in.py
+++ sign_in.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta
 from typing import Optional
 
 import cv2
-import numpy as np
 from arknights_mower.utils.image import loadres, saveimg
 from arknights_mower.utils.log import logger
 from arknights_mower.utils.matcher import ORB
@@ -34,9 +33,6 @@ class SignInSolver(BaseSolver):
         self.back_to_index()
         self.tm = TaskManager()
         self.tm.add("monthly_card", 2024, 6, 1)  # 五周年专享月卡
-        self.tm.add("orundum", 2024, 5, 15)  # 限时开采许可
-        self.tm.add("headhunting", 2024, 5, 15)  # 每日赠送单抽
-        self.tm.add("ann5", 2024, 5, 15)  # 五周年庆典签到活动
         self.tm.add("ep14", 2024, 5, 22)  # 第十四章慈悲灯塔物资领取
 
         self.failure = 0
@@ -78,20 +74,6 @@ class SignInSolver(BaseSolver):
                 else:
                     self.notify("未检测到五周年月卡领取入口！")
                     self.tm.complete("monthly_card")
-            elif self.tm.task == "orundum":
-                if pos := self.find("@hot/orundum/entry"):
-                    self.tap(pos)
-                else:
-                    self.notify("未检测到限时开采许可入口！")
-                    self.tm.complete("orundum")
-            elif self.tm.task == "headhunting":
-                self.tap_index_element("headhunting")
-            elif self.tm.task == "ann5":
-                if pos := self.find("@hot/ann5/entry"):
-                    self.tap(pos)
-                else:
-                    self.notify("未检测到五周年庆典签到活动入口！")
-                    self.tm.complete("ann5")
             elif self.tm.task == "ep14":
                 self.tap_index_element("terminal")
             else:
@@ -110,92 +92,10 @@ class SignInSolver(BaseSolver):
             if self.tm.task == "monthly_card":
                 self.notify("成功领取五周年专享月卡")
                 self.tm.complete("monthly_card")
-            elif self.tm.task == "orundum":
-                self.notify("成功开采合成玉")
-                self.in_progress = False
-                self.tm.complete("orundum")
-            elif self.tm.task == "ann5":
-                self.notify("成功领取五周年庆典签到活动奖励")
             elif self.tm.task == "ep14":
                 self.notify("成功领取理智小样和材料箱子")
                 self.tm.complete("ep14")
             self.tap((960, 960))
-        elif self.find("@hot/orundum/banner"):
-            if self.tm.task == "orundum":
-                if pos := self.find("@hot/orundum/button_start", score=0.6):
-                    self.in_progress = True
-                    self.tap(pos)
-                elif self.find("@hot/orundum/button_complete"):
-                    if self.in_progress:
-                        self.sleep()
-                    else:
-                        self.notify("今天的限时开采活动已经做完了")
-                        self.tm.complete("orundum")
-                        self.back()
-                else:
-                    self.sleep()
-            else:
-                self.back()
-        elif pos := self.find("@hot/headhunting/button"):
-            if self.tm.task == "headhunting":
-                if self.find("@hot/headhunting/banner"):
-                    if self.find("@hot/headhunting/available"):
-                        self.tap(pos, x_rate=0.25)
-                    else:
-                        self.notify("今天的赠送单抽已经抽完了")
-                        self.tm.complete("headhunting")
-                        self.back()
-                elif self.find("@hot/headhunting/banner_exclusive"):
-                    self.tap((1880, 590))
-                else:
-                    self.notify("何以为我卡池已关闭")
-                    self.tm.complete("headhunting")
-                    self.back()
-            else:
-                self.back()
-        elif self.find("@hot/headhunting/dialog"):
-            if self.tm.task == "headhunting":
-                self.tap((1263, 744))
-            else:
-                self.tap((663, 741))
-        elif pos := self.find("skip"):
-            self.ctap(pos)
-        elif self.find(
-            "@hot/headhunting/contract",
-            scope=((1550, 650), (1920, 850)),
-            score=0.1,
-        ):
-            if self.tm.task == "headhunting":
-                self.notify("成功抽完赠送单抽")
-                self.tm.complete("headhunting")
-            self.tap((960, 540))
-        elif self.find("@hot/ann5/banner"):
-            if self.tm.task == "ann5":
-                img = cv2.cvtColor(self.recog.img, cv2.COLOR_RGB2HSV)
-                img = cv2.inRange(img, (8, 150, 0), (8, 255, 255))
-                tpl = np.zeros((100, 100), dtype=np.uint8)
-                tpl[:] = (255,)
-                result = cv2.matchTemplate(img, tpl, cv2.TM_CCORR_NORMED)
-                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-                if max_val > 0.9:
-                    self.in_progress = True
-                    self.ctap((max_loc[0] + 50, max_loc[1] + 50))
-                else:
-                    if not self.in_progress:
-                        self.notify("五周年庆典签到活动奖励已领完")
-                    self.in_progress = False
-                    self.tm.complete("ann5")
-                    self.back()
-            else:
-                self.back()
-        elif self.find("@hot/ann5/phono"):
-            if self.tm.task == "ann5":
-                self.notify("成功领取PhonoR-0小车")
-            self.ctap((960, 540))
-        elif self.find("@hot/ann5/savage"):
-            if self.tm.task == "ann5":
-                self.notify("成功领取暴行皮肤")
-            self.ctap((960, 540))
         elif self.find("terminal_pre"):
             if self.tm.task == "ep14":
                 img = loadres("@hot/ep14/terminal.jpg", True)
