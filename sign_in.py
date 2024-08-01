474e9d1df68f3724a5ece489828b0e1b44a5c4eb
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2024-08-01 22:22:25 +0800

    沉沙赫日签到活动

--- sign_in.py
+++ sign_in.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import cv2
 import numpy as np
+from arknights_mower.utils.email import send_message
 from arknights_mower.utils.graph import SceneGraphSolver
 from arknights_mower.utils.log import logger
 from arknights_mower.utils.scene import Scene
@@ -28,7 +29,7 @@ class TaskManager:
             self.task_list.remove(task)
 
 
-rogue = "rogue"
+brilliant_sun = "brilliant_sun"
 
 
 class SignInSolver(SceneGraphSolver):
@@ -36,7 +37,7 @@ class SignInSolver(SceneGraphSolver):
         logger.info("Start: 签到活动")
         self.scene_graph_navigation(Scene.INDEX)
         self.tm = TaskManager()
-        self.tm.add(rogue, 2024, 7, 19)  # 无终奇语签到活动
+        self.tm.add(brilliant_sun, 2024, 8, 15)  # 沉沙赫日签到活动
 
         self.failure = 0
         self.in_progress = False
@@ -46,8 +47,7 @@ class SignInSolver(SceneGraphSolver):
     def notify(self, msg):
         logger.info(msg)
         self.recog.save_screencap("sign_in")
-        if hasattr(self, "send_message_config") and self.send_message_config:
-            self.send_message(msg, attach_image=self.recog.img)
+        send_message(msg, attach_image=self.recog.img)
 
     def handle_unknown(self):
         self.failure += 1
@@ -68,20 +68,18 @@ class SignInSolver(SceneGraphSolver):
         if self.find("connecting"):
             return self.handle_unknown()
         elif self.recog.detect_index_scene():
-            if self.tm.task == rogue:
-                if pos := self.find("@hot/rogue/entry"):
+            if self.tm.task == brilliant_sun:
+                if pos := self.find("@hot/brilliant_sun/entry"):
                     self.tap(pos)
                 else:
-                    self.notify("未检测到无终奇语签到活动入口！")
-                    self.tm.complete(rogue)
+                    self.notify("未检测到沉沙赫日签到活动入口！")
+                    self.tm.complete(brilliant_sun)
             else:
                 self.tm.complete("back_to_index")
-        elif self.find("@hot/rogue/banner"):
-            if self.tm.task == rogue:
+        elif self.find("@hot/brilliant_sun/banner"):
+            if self.tm.task == brilliant_sun:
                 img = cv2.cvtColor(self.recog.img, cv2.COLOR_RGB2HSV)
-                img1 = cv2.inRange(img, (0, 200, 0), (5, 255, 255))
-                img2 = cv2.inRange(img, (175, 200, 0), (180, 255, 255))
-                img = cv2.bitwise_or(img1, img2)
+                img = cv2.inRange(img, (5, 100, 0), (15, 255, 255))
                 tpl = np.zeros((100, 100), dtype=np.uint8)
                 tpl[:] = (255,)
                 result = cv2.matchTemplate(img, tpl, cv2.TM_CCORR_NORMED)
@@ -91,16 +89,16 @@ class SignInSolver(SceneGraphSolver):
                     self.ctap(va(max_loc, (50, 50)))
                 else:
                     if not self.in_progress:
-                        self.notify("无终奇语签到奖励已领完")
+                        self.notify("沉沙赫日签到奖励已领完")
                     self.in_progress = False
-                    self.tm.complete(rogue)
+                    self.tm.complete(brilliant_sun)
                     self.back()
             else:
                 self.back()
         elif self.find("materiel_ico"):
             self.sleep()
-            if self.tm.task == rogue:
-                self.notify("无终奇语活动签到成功")
+            if self.tm.task == brilliant_sun:
+                self.notify("沉沙赫日活动签到成功")
             else:
                 self.notify("物资领取")
             self.tap((960, 960))
